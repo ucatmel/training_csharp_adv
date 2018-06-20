bf47071e0e327af1e2fd41cbcd67032f6d45248d
ucatmel <ucatmel@gmx.net>
2018-06-20 16:42:04 +0200

    Delegate Erklärungen

---
+++ HalloLinq/HalloLinq/DelegateExplanatiob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloLinq
+{
+    class DelegateExplanatiob
+    {
+        public DelegateExplanatiob()
+        {
+            // Delegate Pointer of Function, with Anonymous Function
+            Action meinDeleaction = delegate () { Console.WriteLine("Hello"); };
+            Action meinDeleactionAno = () => Console.WriteLine("Hello");
+
+            Action<string> meinDeleactionwithParaAno = (string txt) => { Console.WriteLine(txt); };
+            Action<string> meinDeleactionwithParaAno2 = (txt) => Console.WriteLine(txt);
+
+
+            Func<int, int, long> meinCalcAno = (int a, int b) => { return a + b; };
+            Func<int, int, long> meinCalcAno2 = ( a, b) => a + b; 
+
+
+
+        }
+
+    }
+}
