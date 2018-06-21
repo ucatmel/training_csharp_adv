c645ce10d6afd99a839fcde781cbba7948b6af42
ucatmel <ucatmel@gmx.net>
2018-06-21 11:02:22 +0200

    Parallel Threads

--- HalloTPL/HalloTPL/Program.cs
+++ HalloTPL/HalloTPL/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HalloTPL
@@ -10,8 +11,25 @@ namespace HalloTPL
     {
         static void Main(string[] args)
         {
+            //Zaehle();
+            //Zaehle();
+            //Zaehle();
+            //Zaehle();
+
+
+            //Parallel.Invoke(Zaehle, Zaehle, Zaehle, Zaehle, Zaehle, Zaehle, () => Console.WriteLine("HALLO"));
+            Parallel.For(0, 100000, (i) => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}"));
+
             Console.WriteLine("Ende");
             Console.ReadKey();
+        }
+
+        private static void Zaehle()
+        {
+            for (var i = 0; i <10; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}");
+            }
         }
     }
 }
