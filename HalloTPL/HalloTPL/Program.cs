063f198df1cf03abedb64db52b0e2dcaeb566e30
ucatmel <ucatmel@gmx.net>
2018-06-21 11:19:59 +0200

    More Parallel Task

--- HalloTPL/HalloTPL/Program.cs
+++ HalloTPL/HalloTPL/Program.cs
@@ -18,7 +18,16 @@ namespace HalloTPL
 
 
             //Parallel.Invoke(Zaehle, Zaehle, Zaehle, Zaehle, Zaehle, Zaehle, () => Console.WriteLine("HALLO"));
-            Parallel.For(0, 100000, (i) => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}"));
+            //Parallel.For(0, 100000, (i) => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}"));
+
+            Task t1 = new Task(() =>
+             {
+                 Console.WriteLine("T1 gestartet");
+                 Thread.Sleep(400);
+                 Console.WriteLine("T1 fertig");
+             });
+
+            t1.Start();
 
             Console.WriteLine("Ende");
             Console.ReadKey();
