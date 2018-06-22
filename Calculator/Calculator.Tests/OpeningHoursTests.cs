353a8d6beb09d86ea43931014962d2d8519ba807
ucatmel <ucatmel@gmx.net>
2018-06-22 11:34:08 +0200

    TDD Concept

---
+++ Calculator/Calculator.Tests/OpeningHoursTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class OpeningHoursTests
+    {
+        [TestMethod]
+        public void OpeningHours_isOpen()
+        {
+            OpeningHours oh = new OpeningHours();
+
+            Assert.IsFalse(oh.IsOpen( new DateTime(1, 1, 1, 0, 0, 0)));
+            Assert.IsFalse(oh.IsOpen( new DateTime(1, 1, 1, 10, 29, 0)));
+            Assert.IsTrue(oh.IsOpen( new DateTime(1, 1, 1, 10, 30, 0)));
+            Assert.IsTrue(oh.IsOpen( new DateTime(1, 1, 1,18, 59, 0)));
+            Assert.IsFalse(oh.IsOpen(new DateTime(1, 1, 1, 19, 00, 0)));
+
+        }
+
+        [DataTestMethod]
+        [DataRow(2018, 4, 2, 10, 30, true)]//mo
+        [DataRow(2018, 4, 2, 10, 29, false)]//mo
+        [DataRow(2018, 4, 2, 19, 0, false)] //mo
+        [DataRow(2018, 4, 7, 12, 0, true)] //sa
+        [DataRow(2018, 4, 7, 18, 0, false)] //sa
+        [DataRow(2018, 4, 8, 12, 0, false)] //so
+        public void OpeningHours__IsOpen_DataRow(int y, int M, int d, int h, int m, bool result)
+        {
+            var dt = new DateTime(y, M, d, h, m, 0);
+            var oh = new OpeningHours();
+
+            Assert.AreEqual(result, oh.IsOpen(dt));
+        }
+    }
+}
---
+++ Calculator/Calculator/OpeningHours.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class OpeningHours
+    {
+        public bool IsOpen(DateTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
