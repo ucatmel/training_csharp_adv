0d8a3ee9d34004b0be4f7cb696833fe88cec159d
ucatmel <ucatmel@gmx.net>
2018-06-22 14:54:01 +0200

    Add Tracing

--- HalloReflection/HalloReflection/Program.cs
+++ HalloReflection/HalloReflection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -53,8 +54,25 @@ namespace HalloReflection
                 Console.WriteLine(wetter.GetTemperature(DateTime.Now));
             }
 
+            Trace.Listeners.Add(new ConsoleTraceListener(true));
+
+            TraceMessage("Hello");
+
             Console.WriteLine("Ende");
             Console.ReadKey();
         }
+        public static void TraceMessage(string message,
+        [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+        [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+        [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            Trace.WriteLine("message: " + message);
+            Trace.WriteLine("member name: " + memberName);
+            Trace.WriteLine("source file path: " + sourceFilePath);
+            Trace.WriteLine("source line number: " + sourceLineNumber);
+            //throw new EncoderFallbackException();
+        }
+
+
     }
 }
