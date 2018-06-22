b77f365694b2c2919fe9da6a2fa66c84dded416f
ucatmel <ucatmel@gmx.net>
2018-06-22 13:58:33 +0200

    Load Assembly Dynamic and enumerate Types

--- HalloReflection/HalloReflection/Program.cs
+++ HalloReflection/HalloReflection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace HalloReflection
     {
         static void Main(string[] args)
         {
+            int zahl = 5;
+
+            Type t = zahl.GetType();
+
+            var dings = Activator.CreateInstance(t);
+
+            var ass = Assembly.LoadFile(@"C:\Users\ppedv\Source\Repos\training_csharp_adv\Calculator\Calculator\bin\Debug\Calculator.dll");
+
+            foreach (var item in ass.GetTypes())
+            {
+                Console.WriteLine(item.FullName);
+            }
+
             Console.WriteLine("Ende");
             Console.ReadKey();
         }
