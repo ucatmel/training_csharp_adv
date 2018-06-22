09180b78e546c82bdd2c0126070816b79766230f
ucatmel <ucatmel@gmx.net>
2018-06-22 15:39:43 +0200

    ASP.Net Web Example

---
+++ HalloWeb/HalloWeb/Models/Stift.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HalloWeb.Models
+{
+    public class Stift
+    {
+        public int id { get; set; }
+        public virtual Hersteller Hersteller { get; set; }
+        public string Farbe { get; set; }
+        public string Staerke { get; set; }
+        public bool IstWasserfest { get; set; }
+    }
+
+    public class Hersteller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual HashSet<Stift> Stifte { get; set; } = new HashSet<Stift>();
+    }
+}
