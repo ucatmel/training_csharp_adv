f067514d5c309bbcb8a8abaa25a9db5cd04ede90
ucatmel <ucatmel@gmx.net>
2018-06-22 11:47:34 +0200

    TDD Examples

--- Calculator/Calculator.Tests/OpeningHoursTests.cs
+++ Calculator/Calculator.Tests/OpeningHoursTests.cs
@@ -24,9 +24,8 @@ namespace Calculator.Tests
         [DataRow(2018, 4, 2, 10, 29, false)]//mo
         [DataRow(2018, 4, 2, 19, 0, false)] //mo
         [DataRow(2018, 4, 7, 12, 0, true)] //sa
-        [DataRow(2018, 4, 7, 18, 0, false)] //sa
-        [DataRow(2018, 4, 8, 12, 0, false)] //so
-        public void OpeningHours__IsOpen_DataRow(int y, int M, int d, int h, int m, bool result)
+
+        public void OpeningHours_IsOpen_DataRow(int y, int M, int d, int h, int m, bool result)
         {
             var dt = new DateTime(y, M, d, h, m, 0);
             var oh = new OpeningHours();
--- Calculator/Calculator/OpeningHours.cs
+++ Calculator/Calculator/OpeningHours.cs
@@ -10,7 +10,24 @@ namespace Calculator
     {
         public bool IsOpen(DateTime time)
         {
-            throw new NotImplementedException();
+            //if (IsWeekday(time))
+            //{
+            //    return IsWeekdayOpen(time);
+            //}
+            //return true;
+            return IsWeekdayOpen(time);
+        }
+
+        bool IsWeekday(DateTime time)
+        {
+            return time.DayOfWeek.Equals(DayOfWeek.Monday | DayOfWeek.Tuesday | DayOfWeek.Wednesday | DayOfWeek.Thursday | DayOfWeek.Friday);
+
+        }
+        
+
+        bool IsWeekdayOpen(DateTime time)
+        {
+           return  time.TimeOfDay >= new TimeSpan(10,30,0) && time.TimeOfDay < new TimeSpan(19,0,0);
         }
     }
 }
